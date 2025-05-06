f3533150cb1481e21303db2ad5bec1bc74b566ef
Rania Ben Abdellah <rania.benab03@gmail.com>
2025-05-07 00:16:00 +0100

    5  commit

--- src/main/java/ma/enset/hospital/web/PatientController.java
+++ src/main/java/ma/enset/hospital/web/PatientController.java
@@ -1,5 +1,6 @@
 package ma.enset.hospital.web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.enset.hospital.entities.Patient;
 import ma.enset.hospital.repository.PatientRepository;
@@ -8,8 +9,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -36,4 +40,26 @@ public class PatientController {
         return "redirect:/index?page="+page +"&keyword="+keyword;
     }
 
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> lisPatients(){
+        return patientRepository.findAll();
+    }
+
+    @GetMapping("/formPatients")
+    public String formPatient(Model model){
+        model.addAttribute("patient", new Patient());
+        return "formPatients";
+    }
+
+    @PostMapping(path="/save")
+    public String save(Model model, @Valid Patient patient , BindingResult bindingResult){
+        if(bindingResult.hasErrors()) return "formPatients";
+
+        patientRepository.save(patient);
+        return "formPatients";}
 }
